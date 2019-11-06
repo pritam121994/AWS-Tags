cd96aa5a8079a0eef9954d8e6ea749e8947a1253
pritam121994 <57445823+pritam121994@users.noreply.github.com>
2019-11-06 19:12:55 +0530

    Update vol_del_tags.py

--- vol_del_tags.py
+++ vol_del_tags.py
@@ -1,11 +1,13 @@
-# response1 = client.delete_tags(
-#     Resources=[
-#         'vol-id',
-#     ],
-#     Tags=[
-#         {
-#             'Key': 'Name1',
-#             'Value': 'xyz',
-#         },
-#     ],
-# )
+import boto3
+client = boto3.client('ec2')
+response1 = client.delete_tags(
+    Resources=[
+        'vol-id',
+    ],
+    Tags=[
+        {
+            'Key': 'Name1',
+            'Value': 'xyz',
+        },
+    ],
+)
