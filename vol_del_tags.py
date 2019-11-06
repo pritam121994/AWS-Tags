251baf88f5f2a0095d652ae3ae7ad44cb1acc58f
pritam121994 <57445823+pritam121994@users.noreply.github.com>
2019-11-06 19:11:20 +0530

    Create vol_del_tags.py

---
+++ vol_del_tags.py
@@ -0,0 +1,11 @@
+# response1 = client.delete_tags(
+#     Resources=[
+#         'vol-id',
+#     ],
+#     Tags=[
+#         {
+#             'Key': 'Name1',
+#             'Value': 'xyz',
+#         },
+#     ],
+# )
